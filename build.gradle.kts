d9c7606492181fc9485822f1e6c816332b1cda66
Ralf Stephan <ralf@ark.in-berlin.de>
2023-03-21 17:26:38 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+
+plugins {
+    kotlin("multiplatform")
+    id("org.jetbrains.compose")
+}
+
+group = "org.reactome"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    jvm {
+        jvmToolchain(11)
+        withJava()
+    }
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "LitBall"
+            packageVersion = "1.0.0"
+        }
+    }
+}
