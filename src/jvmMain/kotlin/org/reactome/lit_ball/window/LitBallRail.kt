d745887b70e21b37b41badbe6e28f55cfa31bdc0
Ralf Stephan <ralf@ark.in-berlin.de>
2023-04-04 16:30:10 +0200

    refine layout of navigation

--- src/jvmMain/kotlin/org/reactome/lit_ball/window/LitBallRail.kt
+++ src/jvmMain/kotlin/org/reactome/lit_ball/window/LitBallRail.kt
@@ -2,35 +2,22 @@
 
 package org.reactome.lit_ball.window
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.reactome.lit_ball.common.App
 
 @Composable
 fun LitBallRail() {
     var selectedItem by remember { mutableStateOf(0) }
-    val items = listOf("Home", "Search", "Settings")
-    val icons = listOf(Icons.Filled.Home, Icons.Filled.Search, Icons.Filled.Settings)
+    val items = listOf("Info", "Settings", "Exit")
+    val icons = listOf(Icons.Filled.Info, Icons.Filled.Settings, Icons.Filled.ExitToApp)
     val actions = listOf( { App.buttonHome() } , { App.buttonSearch() }, { App.buttonSettings() } )
     NavigationRail {
-        ExtendedFloatingActionButton(onClick = { App.buttonNew() },
-            icon = {
-                Icon(
-                    Icons.Filled.Add,
-                    contentDescription = null,
-                    modifier = Modifier.size(ButtonDefaults.IconSize)
-                )
-            },
-            text = {
-                Text("New Query")
-            })
         items.forEachIndexed { index, item ->
             NavigationRailItem(
                 onClick = {
@@ -42,5 +29,17 @@ fun LitBallRail() {
                 selected = selectedItem == index
             )
         }
+        ExtendedFloatingActionButton(onClick = { App.buttonNew() },
+            icon = {
+                Icon(
+                    Icons.Filled.Add,
+                    contentDescription = null,
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+            },
+            text = {
+                Text("New Query")
+            },
+            modifier = Modifier.padding(horizontal = 0.dp, vertical = 36.dp))
     }
 }
