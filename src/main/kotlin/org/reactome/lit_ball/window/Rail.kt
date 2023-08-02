9b822e02856a59e79190b4b090dd16bca34f9296
Ralf Stephan <ralf@ark.in-berlin.de>
2023-08-02 16:13:40 +0200

    refactor

--- src/main/kotlin/org/reactome/lit_ball/window/Rail.kt
+++ src/main/kotlin/org/reactome/lit_ball/window/Rail.kt
@@ -8,47 +8,55 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun Rail(
     onRailItemClicked: List<() -> Unit>,
-    onNewButtonClicked: () -> Unit
+    onNewButtonClicked: () -> Unit,
+    onExit: () -> Unit,
 ) {
     val selectedItem by remember { mutableStateOf(0) }
-//    val items = listOf("Info", "Settings", "Exit")
-//    val icons = listOf(Icons.Filled.Settings, Icons.Filled.ExitToApp)
-//    val actions = listOf( {  } , { App.buttonSettings() }, { App.buttonExit() } )
+
+    data class RailItem(
+        val text: String,
+        val icon: ImageVector,
+        val actionIndex: Int,
+        val extraAction: (() -> Unit)? = null
+    )
+
+    val items = listOf(
+        RailItem("Info", Icons.Filled.Info, 0),
+        RailItem("Settings", Icons.Filled.Settings, 1),
+        RailItem("Exit", Icons.Filled.ExitToApp, 3, onExit)
+    )
+
     NavigationRail {
-        NavigationRailItem(
-            onClick = onRailItemClicked[0],
-            icon = { Icon(Icons.Filled.Info, null) },
-            label = { Text("Info") },
-            selected = selectedItem == 1
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[1],
-            icon = { Icon(Icons.Filled.Settings, null) },
-            label = { Text("Settings") },
-            selected = selectedItem == 2
-        )
-        NavigationRailItem(
-            onClick = onRailItemClicked[2],
-            icon = { Icon(Icons.Filled.ExitToApp, null) },
-            label = { Text("Exit") },
-            selected = selectedItem == 3
-        )
-        ExtendedFloatingActionButton(onClick = onNewButtonClicked,
-            icon = {
-                Icon(
-                    Icons.Filled.Add,
-                    contentDescription = null,
-                    modifier = Modifier.size(ButtonDefaults.IconSize)
-                )
-            },
-            text = {
-                Text("New Query")
-            },
-            modifier = Modifier.padding(horizontal = 0.dp, vertical = 36.dp))
+        items.forEach { item ->
+            NavigationRailItem(
+                onClick = {
+                    onRailItemClicked[item.actionIndex]()
+                    item.extraAction?.invoke()
+                },
+                icon = { Icon(item.icon, null) },
+                label = { Text(item.text) },
+                selected = selectedItem == item.actionIndex
+            )
+            ExtendedFloatingActionButton(
+                onClick = onNewButtonClicked,
+                icon = {
+                    Icon(
+                        Icons.Filled.Add,
+                        contentDescription = null,
+                        modifier = Modifier.size(ButtonDefaults.IconSize)
+                    )
+                },
+                text = {
+                    Text("New Query")
+                },
+                modifier = Modifier.padding(horizontal = 0.dp, vertical = 36.dp)
+            )
+        }
     }
 }
