46a4a51453315a2dfb00dc60e6fef9be28c9e74e
Ralf Stephan <ralf@ark.in-berlin.de>
2023-08-30 18:05:21 +0200

    poor man's NLP

--- src/main/kotlin/org/reactome/lit_ball/service/NLPService.kt
+++ src/main/kotlin/org/reactome/lit_ball/service/NLPService.kt
@@ -1,20 +1,13 @@
 package org.reactome.lit_ball.service
 
-import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation
-import edu.stanford.nlp.pipeline.StanfordCoreNLP
-import java.util.*
-
 object NLPService {
-    private lateinit var props: Properties
-    private lateinit var pipeline: StanfordCoreNLP
     private var initialized = false
-    private lateinit var punctRegex: Regex
+    private lateinit var notWordRegex: Regex
+    private lateinit var wsRegex: Regex
     fun init() {
         if (initialized) return
-        props = Properties()
-        props.setProperty("annotators", "tokenize,ssplit,pos,lemma")
-        pipeline = StanfordCoreNLP(props)
-        punctRegex = "(?U)\\p{Punct}".toRegex()
+        notWordRegex = Regex("[^\\w\\s]")
+        wsRegex = "\\s+".toRegex()
         initialized = true
     }
 
@@ -22,14 +15,8 @@ object NLPService {
         if (!initialized) init()
         if (s == null) return ""
         var str = ""
-        val document = pipeline.processToCoreDocument(s)
-        document.tokens().forEach {
-            val irrelevantPos = setOf("DT", "IN", "TO", "CC", "PRP$")
-            if (!irrelevantPos.contains(it[PartOfSpeechAnnotation::class.java])) {
-                val lemma = it.lemma()
-                if (!punctRegex.matches(lemma))
-                    str += "$lemma "
-            }
+        s.replace(notWordRegex, "").split(wsRegex).forEach {
+            str += "$it "
         }
         return str
     }
