93bb3319949f5b7038fdb1745dce4d987716a45f
Ralf Stephan <ralf@ark.in-berlin.de>
2023-08-30 18:11:57 +0200

    replace deprecated exec calls

--- src/main/kotlin/org/reactome/lit_ball/util/OpenURI.kt
+++ src/main/kotlin/org/reactome/lit_ball/util/OpenURI.kt
@@ -9,8 +9,8 @@ fun openInBrowser(uri: URI) {
     val desktop = Desktop.getDesktop()
     when {
         Desktop.isDesktopSupported() && desktop.isSupported(Desktop.Action.BROWSE) -> desktop.browse(uri)
-        "mac" in osName -> Runtime.getRuntime().exec("open $uri")
-        "nix" in osName || "nux" in osName -> Runtime.getRuntime().exec("xdg-open $uri")
+        "mac" in osName -> Runtime.getRuntime().exec(arrayOf("open", "$uri"))
+        "nix" in osName || "nux" in osName -> Runtime.getRuntime().exec(arrayOf("xdg-open", "$uri"))
         else -> throw RuntimeException("cannot open $uri")
     }
 }
