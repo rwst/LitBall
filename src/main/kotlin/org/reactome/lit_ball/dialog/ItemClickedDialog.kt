4cfa54c2567e6a70f242e28d2675cea488831349
Ralf Stephan <ralf@ark.in-berlin.de>
2023-09-01 18:01:57 +0200

    fix paper clicked dialog

--- src/main/kotlin/org/reactome/lit_ball/dialog/ItemClickedDialog.kt
+++ src/main/kotlin/org/reactome/lit_ball/dialog/ItemClickedDialog.kt
@@ -1,70 +1,73 @@
 package org.reactome.lit_ball.dialog
 
-import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.text.selection.SelectionContainer
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.reactome.lit_ball.common.PaperList
 
-@OptIn(ExperimentalMaterialApi::class)
 @Suppress("FunctionName")
 @Composable
-// TODO: Fix Scrolling
 internal fun ItemClickedDialog(id: Int, onDoneClicked: () -> Unit) {
-    val lazyListState = rememberLazyListState()
-    AlertDialog(
-        modifier = Modifier
-            .fillMaxSize(fraction = 0.8f)
-            .padding(12.dp),
-        title = {
-            Text(text = "Paper# $id")
-        },
-        text = {
-            Box(
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Row {
-                    LazyColumn(
-                        Modifier.fillMaxSize().padding(end = 12.dp).size(100.dp),
-                        lazyListState
-                    ) {
-                        item {
+    val scrollState = rememberScrollState()
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = Color.Transparent
+    ) {
+        Box(
+            modifier = Modifier
+                .background(color = Color.Black.copy(alpha = 0.5f)),
+            contentAlignment = Alignment.Center,
+        ) {
+            Box(modifier = Modifier.size(500.dp).background(color = Color.White)) {
+                Column (modifier = Modifier.fillMaxWidth()) {
+                    Row (modifier = Modifier.height(440.dp)) {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .verticalScroll(scrollState)
+                                .weight(weight = .1f, fill = false),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
                             SelectionContainer {
                                 Text(
                                     modifier = Modifier
-                                        .padding(16.dp)
-                                        .fillMaxWidth(),
+                                        .padding(16.dp),
                                     text = PaperList.pretty(id),
+                                    fontSize = 14.sp,
                                 )
                             }
                         }
+                        VerticalScrollbar(
+                            modifier = Modifier.align(Alignment.CenterVertically),
+                            adapter = rememberScrollbarAdapter(scrollState)
+                        )
+                    }
+                    ElevatedButton(
+                        onClick = onDoneClicked,
+                        modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .fillMaxHeight()
+                            .padding(12.dp),
+                        elevation = ButtonDefaults.elevatedButtonElevation(4.dp),
+                    ) {
+                        androidx.compose.material3.Text(
+                            text = "Dismiss",
+                            fontSize = 12.sp
+                        )
                     }
                 }
-                VerticalScrollbar(
-                    modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight(),
-                    adapter = rememberScrollbarAdapter(
-                        scrollState = lazyListState
-                    )
-                )
             }
-        },
-        onDismissRequest = {},
-        confirmButton = {
-            TextButton(
-                onClick = { onDoneClicked() },
-            ) {
-                Text("Back")
-            }
-        },
-    )
+        }
+    }
 }
